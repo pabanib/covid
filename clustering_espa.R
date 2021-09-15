efba6f3ac937e9944e28affc8d57ad0eb936f650
Pablo pc <pabanib@hotmail.com>
2021-09-15 20:31:26 -0300

    Emprolijar para subir

--- clustering_espa.R
+++ clustering_espa.R
@@ -13,24 +13,27 @@ positiv_pca <- st_read('Geodabd/positiv_pca.shp')
 
 w_knn6 <- knn_weights(fallec, 6)
 bounds <- fallec['personas']
+n_grupos <-  45
 
-filas <- c('mes2','mes3','mes4','mes5','mes6','mes7','mes8','mes9','mes10','mes11','mes12','mes13','mes14','mes15','mes16')
-filas2 <- c('mes0','mes1','mes2','mes3')
+filas <- c('mes0','mes1','mes2','mes3','mes4','mes5','mes6','mes7','mes8','mes9','mes10','mes11','mes12','mes13','mes14','mes15','mes16')
+filas2 <- c('mes0','mes1')#,'mes2','mes3')
 filas3 <- c('mes0','mes1','mes2','mes3','mes4','mes5','mes6')
+filas4 <- c('mes14')
+guerr <- c('Crm_prs','Crm_prp','Litercy','Donatns','Infants','Suicids')
 clusters <- function(df, nombre,filas){
 datos <- df[filas]
-skater <- skater(5, w_knn6, datos)
-redcap <- redcap(5, w_knn6, datos, "fullorder-completelinkage")
-schc <- schc(5,w_knn6,datos, 'complete')
-try(azpsa <- azp_sa(5,w_knn6, datos, cooling_rate = 0.85, random_seed = 456484))
-try(df['azpsa'] <- azpsa$Clusters)
-try(azptabu <- azp_tabu(5,w_knn6, datos,tabu_length = 10,conv_tabu = 10, random_seed = 456484))
-try(df['azptabu'] <- azptabu$Clusters)
+skater <- skater(n_grupos, w_knn6, datos)
+redcap <- redcap(n_grupos, w_knn6, datos, "fullorder-completelinkage")
+schc <- schc(n_grupos,w_knn6,datos, 'complete')
+#try(azpsa <- azp_sa(5,w_knn6, datos, cooling_rate = 0.85, random_seed = 456484))
+#try(df['azpsa'] <- azpsa$Clusters)
+#try(azptabu <- azp_tabu(5,w_knn6, datos,tabu_length = 10,conv_tabu = 10, random_seed = 456484))
+#try(df['azptabu'] <- azptabu$Clusters)
 
 try(maxpsa <- maxp_sa(w_knn6, datos,bound_variable = bounds,min_bound= 1000000, cooling_rate = 0.85,random_seed = 456484))
 try(df['maxpsa'] <- maxpsa$Clusters)
-try(maxptabu <- maxp_tabu(w_knn6, datos,bound_variable = bounds,min_bound= 1000000 ,tabu_length = 10,conv_tabu = 10,random_seed = 456484))
-try(df['maxptabu'] <- maxptabu$Clusters)
+#try(maxptabu <- maxp_tabu(w_knn6, datos,bound_variable = bounds,min_bound= 1000000 ,tabu_length = 10,conv_tabu = 10,random_seed = 456484))
+#try(df['maxptabu'] <- maxptabu$Clusters)
 
 df['sk'] <- skater$Clusters
 df['redcap'] <- redcap$Clusters
@@ -39,12 +42,12 @@ direc <- paste('df_R/',nombre,'.shp',sep = '')
 st_write(df,direc, delete_layer = T)}
 
 t <- proc.time()
-clusters(fallec, 'fallec',filas)
+clusters(fallec, 'fallec',filas4)
 clusters(fallec_pca, 'fallec_pca',filas2)
-clusters(positiv, 'positiv',filas)
-clusters(positiv_pca, 'positiv_pca',filas2)
-clusters(fall_pos, 'fall_pos',filas3)
-clusters(fall_pos_pca, 'fall_pos_pca',filas2)
+#clusters(positiv, 'positiv',filas)
+#clusters(positiv_pca, 'positiv_pca',filas2)
+#clusters(fall_pos, 'fall_pos',filas3)
+#clusters(fall_pos_pca, 'fall_pos_pca',filas2)
 
 dura <- proc.time()-t
 dura
--- lectura_datos.py
+++ lectura_datos.py
@@ -2,6 +2,10 @@
 """
 Created on Sat Jun  5 10:33:45 2021
 
+El archvo lee los datos del covid realiza la trasnformaciones de las variables
+necesarias para trabajarlas. 
+Agrega los datos geográficos y guarda un archivo shp listo para trabajar
+
 @author: paban
 """
 
@@ -13,29 +17,19 @@ dir_principal = os.getcwd()
 dir_datos = dir_principal+'\\datos'
 covid = pd.read_csv(dir_datos+'/Covid19Casos.csv')
 
-#Arg = gpd.read_file('shape_files/pxdptodatosok.shp')
-
+# Tranforma los datos para poder trabajarlos
 import preproceso as pre 
-
 gen_datos = pre.preparar_datos(covid)
 del(covid)
-
 covid_geo = gen_datos.fit()
-covid_geo.dtypes
 
 import pickle 
-
 with open("datos/covid_geo.pickle", "wb") as f:
     pickle.dump(covid_geo, f)
 
-
+# Agrega condiciones espaciales a los datos
 Arg = gpd.read_file(dir_principal+'/shape_files/pxdptodatosok.shp')
 
-#with open(dir_principal+"/datos/covid_geo.pickle", "rb") as f:
-#    covid_geo = pickle.load(f)
-    
-Arg.query("provincia == 'Tierra del Fuego'").departamen
-Arg.query("provincia == 'Tierra del Fuego'")[['departamen','link']]
 Arg = Arg.drop(Arg.query("link in('94028','94021')").index, axis = 0) 
 Arg['link'] = Arg['link'].astype(int)
 Arg['mujeres'] = Arg['mujeres'].astype(int) 
@@ -67,6 +61,7 @@ for i in index.get_level_values(1):
     covid_periodos[i] = dic
     del(dic)
 
+# controla que no quede ningún valor nulo
 for k in covid_periodos.keys():
     print(covid_periodos[k]['df'].isnull().values.any())
 
@@ -82,6 +77,6 @@ covid['mes'] = covid.mes.astype(str)
 covid = covid.set_index(['link','mes'])
 
 index = covid.index
-#covid = covid.join(index.to_frame())
+
 covid.to_file('datos/covid_periodos.shp', index = True)
 
